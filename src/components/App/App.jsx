7662934ac44d32000e33f2ff518ff39bed3ca221
Aileneboro <a.borovikova89@gmail.com>
2024-04-30 14:17:25 +0300

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,18 @@
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactForm from "../ContactForm/ContactForm";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "./redux/contactsOps";
 import css from "./App.module.css";
 
-const App = () => {
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
@@ -12,6 +21,6 @@ const App = () => {
       <ContactList />
     </div>
   );
-};
+}
 
 export default App;
